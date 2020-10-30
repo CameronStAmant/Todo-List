664bffd4324872151a68b43af534f9295814b41d
Cameron St. Amant <cameronstamant@protonmail.com>
2020-10-30 11:00:11 -0600

    name is updatable

--- src/view/todoSaveEdit.js
+++ src/view/todoSaveEdit.js
@@ -1,14 +1,20 @@
+// import todoView from './todoView';
+
 const todoSaveEdit = () => {
-  'use strict';
   let todoEdit = document.getElementById('todoEdit');
+  console.log('re');
   todoEdit.addEventListener('click', () => {
     let todoInfo = document.querySelectorAll('.todoInfo');
+    console.log('r');
     let getID = todoInfo[1].id;
     let updateID = todoInfo[1].value;
-    console.log(updateID);
     let targetDiv = document.getElementById(getID);
+    let targetButton = document.getElementById(`view-${targetDiv.id}`);
+    targetButton.id = `view-${updateID}`;
     targetDiv.id = updateID;
     targetDiv.innerHTML = updateID;
+    console.log('w');
+    // todoView();
   })
 }
 
--- src/view/todoView.js
+++ src/view/todoView.js
@@ -1,29 +1,37 @@
 const todoView = () => {
   let viewListings = document.getElementById('workspace');
-  let viewArray = [];
-  for (let i = 0; i < viewListings.children.length; i++) {
-    if (viewListings.children[i].className != 'delete-btn') {
-      let b = document.getElementById(viewListings.children[i].id);
-      for (let j = 0; j < b.children.length; j++) {
-        if (b.children[j].className == 'view-todo') {
-          let a = b.children[j].id;
-          viewArray[i] = a;
-          let c = document.getElementById(viewArray[i]);
-          c.addEventListener('click', () => {
-            let viewIt = document.getElementById('todoSpace');
-            let d = viewArray[i].split('-')[1];
-            let viewTodoItem = document.getElementById(d);
-            let todoInfo = document.querySelectorAll('.todoInfo');
-            todoInfo[1].value = viewTodoItem.innerHTML;
-            todoInfo[1].id = viewTodoItem.innerHTML;
-            todoInfo[0].style.display = 'block';
-            todoInfo[1].style.display = 'block';
-            todoInfo[2].style.display = 'block';
-          }) 
+  (function looper = () => {
+    let viewArray = [];
+    for (let i = 0; i < viewListings.children.length; i++) {
+      if (viewListings.children[i].className != 'delete-btn') {
+        let b = document.getElementById(viewListings.children[i].id);
+        for (let j = 0; j < b.children.length; j++) {
+          if (b.children[j].className == 'view-todo') {
+            let a = b.children[j].id;
+            viewArray[i] = a;
+            let c = document.getElementById(viewArray[i]);
+          }
         }
       }
     }
-  }
+  })();
+  c.addEventListener('click', () => {
+    looper();
+    let d = viewArray[i].split('-')[1];
+    let viewTodoItem = document.getElementById(d);
+    let todoInfo;
+    (function () {
+      todoInfo = document.querySelectorAll('.todoInfo');
+    })();
+    viewArray[i] = `mmmview-${viewTodoItem.innerHTML}`;
+
+    todoInfo[1].value = viewTodoItem.innerHTML;
+    todoInfo[1].id = viewTodoItem.innerHTML;
+    todoInfo[0].style.display = 'block';
+    todoInfo[1].style.display = 'block';
+    todoInfo[2].style.display = 'block';
+  }) 
+        
 }
 
 export default todoView
