f8fca8cf1caa54a9eeb2fc3ee6b4e02958c4cb35
Cameron St. Amant <cameronstamant@protonmail.com>
2020-10-31 10:09:43 -0600

    cleanup commented out code

--- src/view/todoSaveEdit.js
+++ src/view/todoSaveEdit.js
@@ -6,12 +6,8 @@ const todoSaveEdit = () => {
     let todoInfo = document.querySelectorAll('.todoInfo');
     let getID = todoInfo[1].id;
     let updateID = todoInfo[1].value;
-    console.log(`getID ${getID}`)
     let targetDiv = document.getElementById(getID);
-    console.log(`updateID ${todoInfo[1]}`);
-    console.log(`old HTML ${targetDiv.innerHTML}`);
     targetDiv.innerHTML = updateID;
-    console.log(`new HTML ${targetDiv.innerHTML}`);
   })
 }
 
