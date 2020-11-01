54d7844ff6d10697d0ae8c21bc5bb768a3e44a40
Cameron St. Amant <cameronstamant@protonmail.com>
2020-10-31 23:51:15 -0600

    fix missing semicolon

--- src/todoCancelButton.js
+++ src/todoCancelButton.js
@@ -3,7 +3,8 @@ import todoButtonActions from "./todoButton";
 const todoCancelButton = () => {
   const todoCancelClick = document.getElementById('cancelTodo');
   const todoButton = document.getElementById('createTodo');
-  const todoTitle = document.getElementById('todoTitle')  const todoDescription = document.getElementById('todoDescription');
+  const todoTitle = document.getElementById('todoTitle');
+  const todoDescription = document.getElementById('todoDescription');
   todoCancelClick.addEventListener('click', () => {
     todoButton.style.display = 'none';
     todoTitle.value = '';
