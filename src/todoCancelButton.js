c48a902a85b8216c9231c4551ca3135e74ef62c9
Cameron St. Amant <cameronstamant@protonmail.com>
2020-11-02 11:12:45 -0700

    Priority is now deselected on cancel

--- src/todoCancelButton.js
+++ src/todoCancelButton.js
@@ -5,8 +5,14 @@ const todoCancelButton = () => {
   const todoButton = document.getElementById('createTodo');
   const todoTitle = document.getElementById('todoTitle');
   const todoDescription = document.getElementById('todoDescription');
+  const todoPriorityOp1 = document.getElementById('titlePriorityOpt1');
+  const todoPriorityOp2 = document.getElementById('titlePriorityOpt2');
+  const todoPriorityOp3 = document.getElementById('titlePriorityOpt3');
   todoCancelClick.addEventListener('click', () => {
     todoButton.style.display = 'none';
+    todoPriorityOp1.checked = false;
+    todoPriorityOp2.checked = false;
+    todoPriorityOp3.checked = false;
     todoTitle.value = '';
     todoDescription.value = '';
   })
