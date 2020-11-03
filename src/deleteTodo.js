d0ec1f8c53b5f45220fbb272ebfa67d1b64b18cc
Cameron St. Amant <cameronstamant@protonmail.com>
2020-11-02 22:03:56 -0700

    delete todo hides todo edit

--- src/deleteTodo.js
+++ src/deleteTodo.js
@@ -11,6 +11,16 @@ const deleteTodo = () => {
           listObj.remove();
           let workspaceArea = document.getElementById('workspace');
           localStorage.setItem(`everything`, `${workspaceArea.innerHTML}`);
+
+          let todoInfo = document.querySelectorAll('.todoInfo');
+          todoInfo[0].style.display = 'none';
+          todoInfo[1].style.display = 'none';
+          todoInfo[2].style.display = 'none';
+          todoInfo[3].style.display = 'none';
+          todoInfo[4].style.display = 'none';
+          todoInfo[5].style.display = 'none';
+          todoInfo[6].style.display = 'none';
+          todoInfo[7].style.display = 'none';
         })
     });
 }
