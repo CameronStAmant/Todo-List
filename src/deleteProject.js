da249e5c7ee3017aa5fdf6e85f934282c2e8c44c
Cameron St. Amant <cameronstamant@protonmail.com>
2020-11-02 22:30:48 -0700

    projects close edit view for todo in their group

--- src/deleteProject.js
+++ src/deleteProject.js
@@ -11,6 +11,22 @@ const deleteProject = () => {
         if (document.getElementById(c) === null) {
           return;
         }
+        let todoInfo = document.querySelectorAll('.todoInfo');
+        console.log(c);
+        let parentNode;
+        if (document.getElementById(`view-${todoInfo[1].id}`) != null) {
+          parentNode = document.getElementById(`view-${todoInfo[1].id}`).parentNode.parentNode.id;
+          if (parentNode == c) {
+            todoInfo[0].style.display = 'none';
+            todoInfo[1].style.display = 'none';
+            todoInfo[2].style.display = 'none';
+            todoInfo[3].style.display = 'none';
+            todoInfo[4].style.display = 'none';
+            todoInfo[5].style.display = 'none';
+            todoInfo[6].style.display = 'none';
+            todoInfo[7].style.display = 'none';
+          }
+        }
         document.getElementById(c).remove();
         b.remove();
         let workspaceArea = document.getElementById('workspace');
